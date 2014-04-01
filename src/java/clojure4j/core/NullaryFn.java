b0461bcb89f0100fa6a128de0e2849dda6717330
Jon Newton <jon.newton@gmail.com>
2014-03-31 22:46:08 -0600

    call() works in NullaryFn

--- src/java/clojure4j/core/NullaryFn.java
+++ src/java/clojure4j/core/NullaryFn.java
@@ -5,6 +5,11 @@ import java.util.function.Supplier;
 public interface NullaryFn<R> extends Supplier<R>, TypedFn<R> {
 
     @Override
+    default R call(){
+        return get();
+    }
+    
+    @Override
     default Object invoke() {
         return get();
     }
